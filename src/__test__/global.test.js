504134b7f3a0c1f04ad5d8b27e720c1d6ad57375
Jonathan Garcia <jona03g97@gmail.com>
2021-03-30 17:18:30 -0600

    Implementando pruebas a promesas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,29 +1,30 @@
-const text = 'Hola Mundo'
-const fruits = ['manzana', 'melon', 'banana']
-test('Debe contener un texto', () => {// la descripción de la prueba generál
-  expect(text).toMatch(/Mundo/)
-})
-
-test('Tenemos una banana en el array', () => {
-  expect(fruits).toContain('banana')
-})
-
-test('Mayor que', () => {
-  expect(10).toBeGreaterThan(7)
-})
-
-test('Verdadero', () => {
-  expect(true).toBeTruthy()
-})
-
-const reverseString = (str, callback) => {//Función que reciba un string e invierta sus valores
-  callback(str.split("").reverse().join(""))
-  //Para invertirlos debemos separar cada caracter, voltearlos, luego unirlos de nuevo en una sola frase
+//Promesa:
+const reverseString = str => {
+  return new Promise((resolve, reject) => {
+    if(!str){
+      reject(Error('Error'))
+    }
+    resolve(str.split('').reverse().join(''))
+  })
 }
 
-test('Probar un callback', () => {
-  //Primero debemos ejecutar el callback, y cuando este se resuelva, ejecutamos el test
-  reverseString('Hola', (str) => {//str sería 'Hola'
-    expect(str).toBe('aloHa')
+test('Probar una promesa', () => {
+  return reverseString('Hola')
+  .then(res => {
+    expect(res).toBe('aloH')
   })
 })
+
+//Async y await
+
+test('Probar async y await', async () => {
+  const string = await reverseString('Hola');
+  expect(string).toBe('aloH')
+})
+
+//Funciones antes o despues de las pruebas
+afterEach(() => console.log('Despues de cada prueba'))
+afterAll(() => console.log('Despues de todas las pruebas'))
+
+beforeEach(() => console.log('Antes de cada prueba'))
+beforeAll(() => console.log('Antes de todas las pruebas'))
