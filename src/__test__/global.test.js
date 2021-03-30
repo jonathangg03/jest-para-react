443b74fd5f316657a264b239e7a52877b98b3591
Jonathan Garcia <jona03g97@gmail.com>
2021-03-30 16:56:31 -0600

    Implementando pruebas en boolean, array, number y callback

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,29 @@
 const text = 'Hola Mundo'
-
+const fruits = ['manzana', 'melon', 'banana']
 test('Debe contener un texto', () => {// la descripción de la prueba generál
   expect(text).toMatch(/Mundo/)
+})
+
+test('Tenemos una banana en el array', () => {
+  expect(fruits).toContain('banana')
+})
+
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(7)
+})
+
+test('Verdadero', () => {
+  expect(true).toBeTruthy()
+})
+
+const reverseString = (str, callback) => {//Función que reciba un string e invierta sus valores
+  callback(str.split("").reverse().join(""))
+  //Para invertirlos debemos separar cada caracter, voltearlos, luego unirlos de nuevo en una sola frase
+}
+
+test('Probar un callback', () => {
+  //Primero debemos ejecutar el callback, y cuando este se resuelva, ejecutamos el test
+  reverseString('Hola', (str) => {//str sería 'Hola'
+    expect(str).toBe('aloHa')
+  })
 })
